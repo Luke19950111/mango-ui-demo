44599038dd60d2d69dd45bdc442d6dce25b1a7c3
liukuai <937613925@qq.com>
2020-03-17 10:26:51 +0800

    mock.js安装测试

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,30 @@
     <div class="page">
         <h2>Home Page</h2>
         <el-button type="primary" @click="testAxios()">测试axios</el-button>
+        <el-button @click="getUser()">用户</el-button>
+        <el-button @click="getMenu()">菜单</el-button>
     </div>
 </template>
 
 <script>
 import axios from 'axios'
+import mock from '@/mock/mock.js'
 export default {
     name: 'Home',
     methods: {
         testAxios(){
             axios.get('http://localhost:8080').then(res=>{
                 alert(res.data)
+            })
+        },
+        getUser(){
+            axios.get('http://localhost:8080/user').then(res=>{
+                alert(JSON.stringify(res.data))
+            })
+        },
+        getMenu(){
+            axios.get('http://localhost:8080/menu').then(res=>{
+                alert(JSON.stringify(res.data))
             })
         }
     }
